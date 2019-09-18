98f3c90dbbd7a777554450b56e064394fb3c54e4
Wendy <52355100+WendyCodesStuff@users.noreply.github.com>
2019-09-18 14:27:29 +0100

    Add files via upload
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

---
+++ 002-EvenFibonacciNumbers/002-EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+namespace EvenFibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const long highestValue = 4000000;
+
+            // Get the sum of the list of even Fibonaccis
+            long total = EnumerateEvenFibonacci(highestValue).Sum();
+
+            Console.WriteLine(total);
+        }
+
+        public static IEnumerable<long> EnumerateEvenFibonacci(long maxVal)
+        {
+            long current = 1;
+            long previous = 0;
+
+            while (current < maxVal)
+            {
+                // return this Fibonacci only if it is even
+                if (current % 2 == 0)
+                    yield return current;
+
+                // Add current to previous to product Fibonacci
+                long temp = previous;
+                previous = current;
+                current += temp;
+            }
+        }
+    }
+}
