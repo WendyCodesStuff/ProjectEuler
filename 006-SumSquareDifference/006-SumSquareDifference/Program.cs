5ad2f6f1695f5fd9c2a147772beb09d8e343efc3
Wendy <52355100+WendyCodesStuff@users.noreply.github.com>
2019-09-21 08:34:25 +0100

    Add files via upload
    The sum of the squares of the first ten natural numbers is: 1^2 + 2^2 + ... + 10^2 = 385
    The square of the sum of the first ten natural numbers is: (1 + 2 + ... + 10)^2 = 55^2 = 3025
    Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.

---
+++ 006-SumSquareDifference/006-SumSquareDifference/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// The sum of the squares of the first ten natural numbers is,
+
+// 1^2 + 2^2 + ... + 10^2 = 385
+// The square of the sum of the first ten natural numbers is,
+
+// (1 + 2 + ... + 10)^2 = 55^2 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+namespace _006_SumSquareDifference
+{
+    class Program
+    {
+        const long max = 100;
+
+        static void Main(string[] args)
+        {
+
+            long difference = SquareOfSums() - SumOfSquares();
+
+            Console.WriteLine(difference);
+        }
+
+        static long SquareOfSums()
+        {
+            long runningTotal = 0;
+
+               // Sum of the square
+            for (long i = 1; i <= max; i++)
+            {
+                runningTotal = runningTotal + i;
+            }
+
+            Console.WriteLine(runningTotal * runningTotal);
+            return (runningTotal * runningTotal);
+            
+        }
+
+        static long SumOfSquares()
+        {
+            long runningTotal = 0;
+
+            // Square of the sum
+            for (long i = 1; i <= max; i++)
+            {
+                runningTotal = runningTotal + (i * i);
+            }
+
+            Console.WriteLine(runningTotal);
+            return runningTotal;
+        }
+
+    }
+}
