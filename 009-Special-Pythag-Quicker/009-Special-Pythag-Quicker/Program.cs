5fdeb3f42c734cb65822a41b67e9bb3820499613
Wendy <52355100+WendyCodesStuff@users.noreply.github.com>
2019-09-23 16:38:08 +0100

    Add files via upload
    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
    
    a^2 + b^2 = c^2
    For example, 32 + 42 = 9 + 16 = 25 = 52.
    
    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
    Find the product abc.
    
    For more discussion on this and all my Project Euler code, see my blog: https://coderoo.joomla.com/index.php/euler-maths/15-special-pythagorean-triplet

---
+++ 009-Special-Pythag-Quicker/009-Special-Pythag-Quicker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Special_Pythag_Quicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+
+            PrintTriplet();
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Time Taken: " + (end-start).TotalSeconds.ToString() + " seconds");
+        }
+
+        static void PrintTriplet()
+        {
+            for (long c = 1; c <= 1000; c++)
+            {
+                long cc = c * c;
+                for (long a = 1; a <= (1000 - c); a++)
+                {
+                    long aa = a * a;
+                    long b = 1000 - c - a;
+                    if (b > 0)
+                    {
+                        // if a^2 + b^2 = c^2
+                        if ((aa) + (b * b) == (cc))
+                        {
+                            //if (a + b + c == 1000)
+                            //{
+                                Console.WriteLine("Product " + a + " * " + b + " * " + c + " = " + (a * b * c));
+                                return;
+                            //}
+                            //else
+                            //{
+                            //    if (a + b + c > 1000)
+                            //        break;
+                            //}
+                        }
+                    }
+                   
+                }
+            }
+        }
+    }
+}
