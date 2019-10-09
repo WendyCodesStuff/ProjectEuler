6bb79ff4f3c05cc53052d18117e1edecb6219c30
Wendy <52355100+WendyCodesStuff@users.noreply.github.com>
2019-10-09 17:41:57 +0100

    Add files via upload
    215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
    
    What is the sum of the digits of the number 21000?

---
+++ 16-Power-Digit_sum/16-Power-Digit_sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace Power_Digit_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get 2^1000 and convert to string
+            BigInteger bigNumber = BigInteger.Pow(2, 1000);
+            string stringBigNum = bigNumber.ToString();
+            long lengthOfBigNum = stringBigNum.Length;
+            int counter = 0;
+            long total = 0;
+
+            for (int i = 0; i < lengthOfBigNum; i++)
+            {
+                // Get next number in bigNumber string and add to total
+                total = total + Int32.Parse(stringBigNum.Substring(counter, 1));
+                counter++;
+            }
+
+            // What's the total
+            Console.WriteLine("2^1000 = " + bigNumber + ". Digits added together makes: " + total);
+        }
+    }
+}
