fac9a672aba72b46dfca11321a19dde29994008c
Wendy <52355100+WendyCodesStuff@users.noreply.github.com>
2019-11-06 15:16:21 +0000

    Add files via upload
    n! means n × (n − 1) × ... × 3 × 2 × 1
    
    For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
    and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
    
    Find the sum of the digits in the number 100!

---
+++ 020-FactorialDigitSum/020-FactorialDigitSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+//n! means n × (n − 1) × ... × 3 × 2 × 1
+
+//For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+//and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+//Find the sum of the digits in the number 100!
+
+namespace FactorialDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger factorial = 1;
+
+            // Find 100!
+            for(int i = 100; i > 0;  i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+
+            // Covert to a string
+            string stringFactorial = factorial.ToString();
+            long runningTotal = 0;
+
+            // Add the digits
+            for(int i = 0; i <= stringFactorial.Length -1; i++)
+            {
+                // Add next number
+                runningTotal += Convert.ToInt64(stringFactorial.Substring(i, 1));
+            }
+
+            Console.WriteLine("Total = " + runningTotal);
+        }
+    }
+}
